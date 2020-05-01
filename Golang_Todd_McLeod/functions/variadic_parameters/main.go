0887bdc3a324a93571d9929fcf44e6fbc09cb587
Rob Knight <robknight@monzo.com>
2020-05-02 00:44:26 +0100

    unfurling slices in variadic parameter arguments

--- Golang_Todd_McLeod/functions/variadic_parameters/main.go
+++ Golang_Todd_McLeod/functions/variadic_parameters/main.go
@@ -5,11 +5,20 @@ import (
 )
 
 func main() {
-	sum := sum(10, 2, 3)
-	fmt.Println("double the sum is:", 2*sum)
+	s := sum(10, 2, 3)
+	fmt.Println("double the sum is:", 2*s)
+
+	// unfurl a slice
+	nums := []int{1, 2, 3, 4, 5, 7, 8}
+	numsSum := sum(nums...)
+	fmt.Println("double the sum is:", 2*numsSum)
+
+	// variadic means 0 or more - so can pass nothing to function
+	fmt.Println("can give 0 values to sum to get:", sum())
 }
 
 // variadic parameter - takes any number values of type int and stores them in a slice
+// variadic parameter has to be the last paramater, cannot put normal parameters after
 func sum(x ...int) int {
 	fmt.Println(x)
 	fmt.Printf("%T\n", x)
