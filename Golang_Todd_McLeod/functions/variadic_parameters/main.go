54d2bdc6117d096c36bfdf6621516d79ece7a71a
Rob Knight <robknight@monzo.com>
2020-05-01 01:30:02 +0100

    variadic parameters in functions

--- Golang_Todd_McLeod/functions/variadic_parameters/main.go
+++ Golang_Todd_McLeod/functions/variadic_parameters/main.go
@@ -5,5 +5,18 @@ import (
 )
 
 func main() {
-	fmt.Println("hello")
+	sum := sum(10, 2, 3)
+	fmt.Println("double the sum is:", 2*sum)
+}
+
+// variadic parameter - takes any number values of type int and stores them in a slice
+func sum(x ...int) int {
+	fmt.Println(x)
+	fmt.Printf("%T\n", x)
+	s := 0
+	for i, v := range x {
+		s += v
+		fmt.Println("for item number", i, "current sum:", s)
+	}
+	return s
 }
